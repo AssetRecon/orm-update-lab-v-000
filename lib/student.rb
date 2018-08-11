c4562f60d75cdde86a4134127b093a5994987fb8
Edsel Caprice <caprice@mail.usf.edu>
2018-08-11 11:59:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,9 +38,13 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-def self.create(name, grade)
-  student = Student.new(name, grade)
-  student.save
-end
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+  
+  def self.new_from_db(row)
+    Student.new(row[1],row[2],row[0])
+  end
 
 end
